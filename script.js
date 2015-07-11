30da587390596b24127f97204bccbedee801b557
Tyler Eich <eichtyler@gmail.com>
2015-07-10 20:51:57 -0400

    Background scrolling

--- script.js
+++ script.js
@@ -5,6 +5,60 @@
 // var charSpan = document.createElement( 'span' );
 // charSpan.className = 'char';
 
+function backgroundScroll() {
+  var canvas = document.querySelector( '#background' );
+  var ctx = canvas.getContext( '2d' );
+  
+  var y = 0;
+
+  var bg = new Image();
+
+  bg.addEventListener( 'load', function drawBackground() {
+    console.time( 'render' );
+//     console.log( 'Loaded' );
+//     console.log( canvas.width, canvas.height );
+    var width = bg.naturalWidth;
+    var height = bg.naturalHeight;
+    
+    ctx.drawImage( bg, -600, -y * 0.5, width * 0.7, height * 0.7 );
+
+    y++;
+
+    console.timeEnd( 'render' );
+
+    window.setTimeout(
+      requestAnimationFrame.bind( window, drawBackground ),
+      100
+    );
+  }, false );
+
+  bg.src = 'blurred-background.png';
+}
+
+function backgroundImageScroll() {
+  var bg = document.querySelector( '#background' );
+  
+  var y = 0;
+
+  function moveBackground() {
+    console.time( 'render' );
+    console.log( y );
+//     console.log( canvas.width, canvas.height );
+    
+    bg.style.backgroundPosition = '50% -' + y + 'px';
+    y++;
+
+    console.timeEnd( 'render' );
+
+    window.setTimeout(
+      requestAnimationFrame.bind( window, moveBackground ),
+      200
+    );
+  }
+  
+  moveBackground();
+}
+
 function typeString( string, i, element ) {
   if ( i >= string.length ) {
     return;
@@ -112,6 +166,10 @@ window.addEventListener( 'load', function() {
   setTimeout( function() {
     typeString( 'Hello world!', 0, document.querySelector( '#hello-world' ) );
   }, 1000 );
+  
+//   backgroundImageScroll();
+
+//   backgroundScroll();
 
   // $( '#background' ).plaxify();
   // $( '#hello-world' ).plaxify();
