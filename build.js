200030131d3eff8f21a14a1a0d110fa2dc3279f4
Tyler Eich <eichtyler@gmail.com>
2015-07-13 17:43:33 -0400

    build process, minified index.html

--- build.js
+++ build.js
@@ -1,21 +1,29 @@
 var fs = require( 'fs' );
 
 var autoprefixer = require( 'autoprefixer-core' );
-var cssnano = require('cssnano')
+// var CleanCss = require( 'clean-css' );
 var postcss = require( 'postcss' );
 
-var uglifyjs = require( 'uglify-js2' );
+// var uglifyjs = require( 'uglify-js' );
+
+var htmlMinify = require('html-minifier').minify;
 
 
-function prefixAndMinifyCss() {
+var scriptJs = fs.readFileSync( 'script.js', 'utf-8' );
+
+
+function prefixCss() {
   var styleCss = fs.readFileSync( 'style.css', { encoding: 'utf-8' } );
 
-  return postcss([ autoprefixer, cssnano() ]).process( styleCss )
+  return postcss([ autoprefixer ]).process( styleCss )
     .then( function ( result ) {
       result.warnings().forEach( function ( warn ) {
         console.warn( warn.toString() );
       });
 
+      // var minified = new CleanCss().minify( result.css ).styles;
+
+      // console.log( minified );
       return result.css;
     });
 }
@@ -29,27 +37,63 @@ function minifyJs() {
 
 var html = fs.readFileSync( 'dev.html', { encoding: 'utf-8' } );
 
+// console.log( prefixAndMinifyCss() );
 
-prefixAndMinifyCss()
+prefixCss()
   .then(
     function( css ) {
       html = html.replace(
         '<link rel="stylesheet" href="style.css">',
         '<style>' + css + '</style>'
       );
-    }
-  )
-  .then( minifyJs )
-  .then(
-    function( js ) {
+
       html = html.replace(
         '<script src="script.js"></script>',
-        '<script>' + js + '</script>'
+        '<script>' + scriptJs + '</script>'
       );
+
+      html = htmlMinify( html, {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        minifyCSS: true
+      });
+
+      fs.writeFileSync( 'index.html', html );
     }
   )
-  .then(
-    function() {
-      console.log( html );
-    }
-  )
+
+// prefixAndMinifyCss()
+//   .then(
+//     function( css ) {
+//       // console.log( css );
+//       html = html.replace(
+//         '<link rel="stylesheet" href="style.css">',
+//         '<style>' + css + '</style>'
+//       );
+//     }
+//   )
+//   .then( minifyJs )
+//   .then(
+//     function( js ) {
+//       html = html.replace(
+//         '<script src="script.js"></script>',
+//         '<script>' + js + '</script>'
+//       );
+//     }
+//   )
+//   .then(
+//     function() {
+//       html = minify( html, {
+//         removeAttributeQuotes: true
+//       });
+//
+//       console.log( html );
+//     }
+//   ).then(
+//     function() {
+//       console.log( 'finished' );
+//     }
+//   )
+
+// setTimeout( function() {}, 5000 );
