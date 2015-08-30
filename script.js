93fdb65adf3eeceffaef1cf585c929a474fedc18
Tyler Eich <eichtyler@gmail.com>
2015-08-29 21:00:01 -0400

    Experimental light color palette

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-function typeString( string, i, element ) {
-  if ( i >= string.length ) {
-    return;
-  }
+var KEYSTROKE_DELAY = 100;
 
-  var amendment;
+// Syntax sugar. I hate typing fn.bind( null, ... )
+function bond() {
+  var args = Array.prototype.slice.call( arguments );
+  var fn = args.shift();
+  args.unshift( null );
 
-  var char = string[ i ];
+  return fn.bind.apply( fn, args );
+}
 
+// Promise wrapper for setTimeout
+function wait( ms ) {
+  return new Promise( function( resolve, reject ) {
+    setTimeout( resolve, ms );
+  });
+}
+
+
+function typeChar( char, element ) {
   element.innerText += char;
+}
+function typeString( string, element, cb ) {
+  var chars = string.split( '' );
 
-  var delay = Math.random() * 200 + 100;
+  var promise = chars.reduce( function( promise, char ) {
+    var delay = KEYSTROKE_DELAY + Math.random() * KEYSTROKE_DELAY / 3;
+    var nextPromise = promise.then( bond( wait, delay ) )
+      .then( bond( typeChar, char, element ) );
+    
+    return nextPromise;
+  }, Promise.resolve() );
 
-  setTimeout( typeString.bind( null, string, ++i, element ), delay );
+  return promise;
 }
 
 function beginTyping() {
-  setTimeout( function() {
-    typeString( 'Hello world.', 0, document.querySelector( '#hello-world' ) );
-  }, 1000 );
+  var hello = document.querySelector( '#hello-world' );
+  wait( 1000 ).then( bond( typeString, 'Hello world', hello ) )
+    .then( bond( wait, 750 ) )
+    .then( function() {
+      var tyler = document.querySelector( '#tyler-eich' );
+      tyler.classList.add( 'enter' );
+    });
 }
 if ( document.readyState !== 'loading' ) {
   beginTyping();
